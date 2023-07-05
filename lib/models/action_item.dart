9169a05931b23b9f0aee4fb49c2da974190c7998
Jason Tran <tqdung@humaxdigital.com>
2023-07-05 15:37:17 +0700

    Update Custom Dropdown

--- lib/models/action_item.dart
+++ lib/models/action_item.dart
@@ -28,117 +28,91 @@ class Item {
   final Color optionsBorderColor;
 }
 
-final List<Item> menuItems = [
-  Item(
-      name: 'Up',
-      uid: '1',
-      backgroundColor: const Color(0xFF7DBF32),
-      borderColor: const Color(0xFF6A9E31),
-      asset: CustomIcons.up_big,
-      details: ["Forward", "Backward"],
-      options: List.generate(7, (index) => index + 1),
-      detailsBorderColor: const Color(0xFF95D13F),
-      optionsBorderColor: const Color(0xFFE4E757),
-      optionsDropDownColor: const Color(0xFFB6BB17),
-      optionsColor: const Color(0xFFCDDB00)),
-  Item(
-      name: 'Back',
-      uid: '1',
-      backgroundColor: Color(0xFFFB7D9D),
-      borderColor: Color(0xFFD66A8B),
-      asset: CustomIcons.ccw,
-      details: ["To the left", "To the right"],
-      options: List.generate(7, (index) => index + 1),
-      detailsBorderColor: const Color(0xFFFAA7C1),
-      optionsBorderColor: const Color(0xFFF6D3DE),
-      optionsDropDownColor: const Color(0xFFD48DA3),
-      optionsColor: const Color(0xFFFFAFC8)),
-  const Item(
-      name: 'Light',
-      uid: '1',
-      backgroundColor: Color(0xFF6D70A6),
-      borderColor: Color(0xFF7272C0),
-      asset: CustomIcons.lightbulb_1,
-      options: <Color>[Colors.red, Colors.yellow, Colors.blue, Colors.green],
-      detailsBorderColor: const Color(0xFF8080D8),
-      optionsBorderColor: const Color(0xFFDFD0F2),
-      optionsDropDownColor: const Color(0xFFDFD0F2),
-      optionsColor: const Color(0xFFBFAFD4)),
-  const Item(
-      name: 'Sound',
-      uid: '1',
-      backgroundColor: Color(0xFF7272C0),
-      borderColor: Color(0xFF6D70A6),
-      asset: CustomIcons.volume,
-      options: ["Hi", "Goodbye"],
-      detailsBorderColor: const Color(0xFF8080D8),
-      optionsBorderColor: const Color(0xFFDFD0F2),
-      optionsDropDownColor: const Color(0xFFAA9DBC),
-      optionsColor: const Color(0xFFBFAFD4)),
-  Item(
-      name: 'Loop',
-      uid: '1',
-      backgroundColor: Color(0xFFFFA400),
-      borderColor: Color(0xFFDE8A10),
-      asset: CustomIcons.loop,
-      options: List.generate(7, (index) => index + 1),
-      detailsBorderColor: const Color(0xFFFFB845),
-      optionsBorderColor: const Color(0xFFFFE497),
-      optionsDropDownColor: const Color(0xFFD8AE48),
-      optionsColor: const Color(0xFFFFD34C))
-];
+final upItem = Item(
+    name: 'Up',
+    uid: '1',
+    backgroundColor: const Color(0xFF7DBF32),
+    borderColor: const Color(0xFF6A9E31),
+    asset: CustomIcons.up_big,
+    details: ["Forward", "Backward"],
+    options: List.generate(7, (index) => index + 1),
+    detailsBorderColor: const Color(0xFF95D13F),
+    optionsBorderColor: const Color(0xFFE4E757),
+    optionsDropDownColor: const Color(0xFFB6BB17),
+    optionsColor: const Color(0xFFCDDB00));
 
-// enum MenuItems {
-//   up = Item(
-//       name: 'Up',
-//       backgroundColor: const Color(0xFF7DBF32),
-//       borderColor: const Color(0xFF6A9E31),
-//       asset: CustomIcons.up_big,
-//       details: ["Forward", "Backward"],
-//       options: List.generate(7, (index) => index + 1),
-//       detailsBorderColor: const Color(0xFF95D13F),
-//       optionsBorderColor: const Color(0xFFE4E757),
-//       optionsDropDownColor: const Color(0xFFB6BB17),
-//       optionsColor: const Color(0xFFCDDB00)),
-//   back(
-//       name: 'Back',
-//       backgroundColor: Color(0xFFFB7D9D),
-//       borderColor: Color(0xFFD66A8B),
-//       asset: CustomIcons.ccw,
-//       details: ["To the left", "To the right"],
-//       options: List.generate(7, (index) => index + 1),
-//       detailsBorderColor: const Color(0xFFFAA7C1),
-//       optionsBorderColor: const Color(0xFFF6D3DE),
-//       optionsDropDownColor: const Color(0xFFD48DA3),
-//       optionsColor: const Color(0xFFFFAFC8)),
-//   light(
-//       name: 'Light',
-//       backgroundColor: Color(0xFF6D70A6),
-//       borderColor: Color(0xFF7272C0),
-//       asset: CustomIcons.lightbulb_1,
-//       options: <Color>[Colors.red, Colors.yellow, Colors.blue, Colors.green],
-//       detailsBorderColor: const Color(0xFF8080D8),
-//       optionsBorderColor: const Color(0xFFDFD0F2),
-//       optionsDropDownColor: const Color(0xFFDFD0F2),
-//       optionsColor: const Color(0xFFBFAFD4)),
-//   sound(
-//       name: 'Sound',
-//       backgroundColor: Color(0xFF7272C0),
-//       borderColor: Color(0xFF6D70A6),
-//       asset: CustomIcons.volume,
-//       options: ["Hi", "Goodbye"],
-//       detailsBorderColor: const Color(0xFF8080D8),
-//       optionsBorderColor: const Color(0xFFDFD0F2),
-//       optionsDropDownColor: const Color(0xFFAA9DBC),
-//       optionsColor: const Color(0xFFBFAFD4)),
-//   loop(
-//       name: 'Loop',
-//       backgroundColor: Color(0xFFFFA400),
-//       borderColor: Color(0xFFDE8A10),
-//       asset: CustomIcons.loop,
-//       options: List.generate(7, (index) => index + 1),
-//       detailsBorderColor: const Color(0xFFFFB845),
-//       optionsBorderColor: const Color(0xFFFFE497),
-//       optionsDropDownColor: const Color(0xFFD8AE48),
-//       optionsColor: const Color(0xFFFFD34C))
-// };
+final backItem = Item(
+    name: 'Back',
+    uid: '1',
+    backgroundColor: Color(0xFFFB7D9D),
+    borderColor: Color(0xFFD66A8B),
+    asset: CustomIcons.ccw,
+    details: ["To the left", "To the right"],
+    options: List.generate(7, (index) => index + 1),
+    detailsBorderColor: const Color(0xFFFAA7C1),
+    optionsBorderColor: const Color(0xFFF6D3DE),
+    optionsDropDownColor: const Color(0xFFD48DA3),
+    optionsColor: const Color(0xFFFFAFC8));
+
+const lightItem = Item(
+    name: 'Light',
+    uid: '1',
+    backgroundColor: Color(0xFF7272C0),
+    borderColor: Color(0xFF6D70A6),
+    asset: CustomIcons.lightbulb_1,
+    options: ["red", "yellow", "blue", "green"],
+    detailsBorderColor: const Color(0xFF8080D8),
+    optionsBorderColor: const Color(0xFFDFD0F2),
+    optionsDropDownColor: const Color(0xFFAA9DBC),
+    optionsColor: const Color(0xFFBFAFD4));
+
+const soundItem = Item(
+    name: 'Sound',
+    uid: '1',
+    backgroundColor: Color(0xFF7272C0),
+    borderColor: Color(0xFF6D70A6),
+    asset: CustomIcons.volume,
+    options: ["Hi", "Goodbye"],
+    detailsBorderColor: const Color(0xFF8080D8),
+    optionsBorderColor: const Color(0xFFDFD0F2),
+    optionsDropDownColor: const Color(0xFFAA9DBC),
+    optionsColor: const Color(0xFFBFAFD4));
+
+final loopItem = Item(
+    name: 'Loop',
+    uid: '1',
+    backgroundColor: Color(0xFFFFA400),
+    borderColor: Color(0xFFDE8A10),
+    asset: CustomIcons.loop,
+    options: List.generate(7, (index) => index + 1),
+    detailsBorderColor: const Color(0xFFFFB845),
+    optionsBorderColor: const Color(0xFFFFE497),
+    optionsDropDownColor: const Color(0xFFD8AE48),
+    optionsColor: const Color(0xFFFFD34C));
+final endLoopItem = Item(
+    name: 'EndLoop',
+    uid: '1',
+    backgroundColor: Color(0xFFFFA400),
+    borderColor: Color(0xFFDE8A10),
+    asset: CustomIcons.loop,
+    options: List.generate(7, (index) => index + 1),
+    detailsBorderColor: const Color(0xFFFFB845),
+    optionsBorderColor: const Color(0xFFFFE497),
+    optionsDropDownColor: const Color(0xFFD8AE48),
+    optionsColor: const Color(0xFFFFD34C));
+
+final List<Item> menuItems = [upItem, backItem, lightItem, soundItem, loopItem];
+
+Color getColor(String color) {
+  if (color == "red") {
+    return Colors.red;
+  } else if (color == "yellow") {
+    return Colors.yellow;
+  } else if (color == "blue") {
+    return Colors.blue;
+  } else if (color == "green") {
+    return Colors.blue;
+  } else {
+    return Colors.white;
+  }
+}
